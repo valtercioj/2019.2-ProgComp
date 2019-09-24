628142e56338568d01ebd2a2d9dd6a4c401d4345
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2019-09-23 21:51:49 -0300

    Atualização de Repositório

---
+++ 20190923 - Avaliação #01 - Unidade I - Prova A1 (Teórica)/prova_2019_2_q_01_a_gabarito.py
@@ -0,0 +1,25 @@
+A = 0
+B = -20
+
+# Imprimindo os valores iniciais das variáveis antes do laço
+print('  A  |  B  |  C ')
+print('================')
+print(' {0:3} | {1:3} | '.format(A,B))
+print('================')
+
+while (A > B):
+   if (A > B):
+     B = 2 * A + B
+     A += 1
+
+   if (B < 0):
+     C = (B-1) ** 2
+   else:
+     C = A ** 2
+
+   # Imprimindo os valores das variáveis a cada iteração do laço
+   print(' {0:3} | {1:3} | {2:3} '.format(A,B,C))
+   print('----------------')
+
+# Imprimindo o valor das variáveis solicitadas na questão
+print('Resposta: A = {0} / B = {1} / C = {2}'.format(A,B,C))
---
+++ 20190923 - Avaliação #01 - Unidade I - Prova A1 (Teórica)/prova_2019_2_q_02_b_gabarito.py
@@ -0,0 +1,21 @@
+A = 0
+B = 1
+C = A + B
+D = 10
+
+# Imprimindo os valores iniciais das variáveis antes do laço
+print('  A  |  B  |  C  |  D ')
+print('=======================')
+print(' {0:3} | {1:3} | {2:3} | {3:3}  '.format(A,B,C,D))
+print('=======================')
+
+for X in range(0, D-1):
+   C = A - B
+   A = B
+   B = C
+
+   # Imprimindo os valores das variáveis a cada iteração do laço
+   print(' {0:3} | {1:3} | {2:3} | {3:3}  '.format(A,B,C,D))
+   print('-----------------------')
+   
+print(C)
